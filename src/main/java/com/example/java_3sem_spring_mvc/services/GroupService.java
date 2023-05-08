c526cb6f79d2fbc36acdee7ae527cc0638684ce9
Evgeny <Max32rus37@yandex.ru>
2023-05-08 14:11:53 +0300

    prac19

--- src/main/java/com/example/java_3sem_spring_mvc/services/GroupService.java
+++ src/main/java/com/example/java_3sem_spring_mvc/services/GroupService.java
@@ -3,32 +3,40 @@ package com.example.java_3sem_spring_mvc.services;
 import com.example.java_3sem_spring_mvc.model.Group;
 import com.example.java_3sem_spring_mvc.repositories.GroupRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
+@Slf4j
 public class GroupService {
     private final GroupRepository groupRepository;
 
 
     public List<Group> filterGroups(Long id, String name) {
         if (id == null && name == null) {
+            log.info("Группы выведены без фильтра");
             return groupRepository.findAll();
         } else {
+            log.info("Группы выведены с фильтром: id=" + id + " name=" + name);
             return groupRepository.findByIdOrGroupNameContaining(id, name);
         }
     }
 
     public void createGroup(Group group) {
+        log.info("Группа создана");
         groupRepository.save(group);
     }
 
     public void removeGroup(Long index) {
+        log.info("Группа удалена id=: " + index);
         groupRepository.deleteById(index);
     }
 
     public String getGroup(Long index){
+        log.info("Зайшли в группу по id=: " + index);
         return groupRepository.findById(index).toString();
     }
 
--- src/main/java/com/example/java_3sem_spring_mvc/services/StudentService.java
+++ src/main/java/com/example/java_3sem_spring_mvc/services/StudentService.java
@@ -4,32 +4,41 @@ import com.example.java_3sem_spring_mvc.model.Group;
 import com.example.java_3sem_spring_mvc.model.Student;
 import com.example.java_3sem_spring_mvc.repositories.StudentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
+@Slf4j
 public class StudentService {
     private final StudentRepository studentRepository;
 
     public List<Student> filterStudents(Long id, String firstName, String lastName, String middleName, Group group) {
         if (id == null && firstName == null && lastName == null && group == null && middleName == null) {
+            log.info("Студенты выведены без фильтра");
             return studentRepository.findAll();
         } else {
+            log.info("Студенты выведены с фильтром id=" + id + " name=" + firstName+" "+ lastName+" "+middleName + " groups=" + group);
             return studentRepository.findByIdOrFirstNameContainingOrLastNameContainingOrMiddleNameContainingOrGroup(id, firstName, lastName, middleName, group);
         }
     }
 
     public void createStudent(Student student) {
+
+        log.info("Студент создан");
         studentRepository.save(student);
     }
 
     public void removeStudent(Long index) {
+
+        log.info("Студент удалён по id: " + index);
         studentRepository.deleteById(index);
     }
 
     public String getStudent(Long index){
+        log.info("Зайшли на страничку студента по id=: " + index);
         return studentRepository.findById(index).toString();
 
     }
